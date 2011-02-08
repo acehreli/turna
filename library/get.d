f7ce8bb1644cf917e36f4c0963b24d9164d38abc
canalpay <canalpayciftci@gmail.com>
2011-02-08 19:03:33 +0200

    added get.d in library

---
+++ library/get.d
@@ -0,0 +1,62 @@
+/*
+ * This module for get method.
+ * 
+ * */
+
+module library.get;
+
+import std.process; //  getenv
+import std.string;
+
+/*
+ * BUG: 
+ * 
+ * get is very slow. Because we used "~=" operator in fixedString function.
+ * 
+ */
+
+string[string] get()
+{
+    string[string] endGet;
+    string get = getenv("QUERY_STRING");
+
+    endGet=fixedString(get);
+ 
+    
+    
+    return endGet;
+}
+
+string[string] fixedString(string get)
+{
+    get= replace(get, "+", " ");
+    string[string] endGet;
+
+    string[] fixedGet1=split(get, "&"); //
+
+    string[][] fixedGet2;
+
+    foreach(fixedGet;fixedGet1) {
+
+        fixedGet2~=split(fixedGet,"=");        
+        
+    }
+    
+    for(int a=0;a<fixedGet1.length;++a) {
+        for(int b=1;b<fixedGet2[a].length;++b) {
+            //endGet[fixedGet2[a][0]]~=fixedGet2[a][b+1];
+            endGet[fixedGet2[a][0]]~=fixedGet2[a][b];
+        }                
+    }
+    
+    return endGet;
+}
+unittest
+{
+    string[string] deneme;
+    
+    deneme=fixedString("hayvan=at&derece=Orta&sevilen+hayvan=ğüsçü&ikinci+d%C3%BC%C4%9Fme=Ba%C5%9Fka+D%C3%BC%C4%9Fme");
+    assert(deneme["hayvan"]=="at");
+    assert(deneme["sevilen hayvan"]=="ğüşçü");
+
+}
