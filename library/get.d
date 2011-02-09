83af141e75c2e63275cead0072de249176338ca9
canalpay <canalpayciftci@gmail.com>
2011-02-09 19:59:39 +0200

    deleted library.get.fixedString

--- library/get.d
+++ library/get.d
@@ -8,6 +8,7 @@ module library.get;
 import std.process; //  getenv
 import std.string;
 import library.convertToUTF8;
+import library.fixedString;
 
 /*
  * BUG: 
@@ -29,36 +30,3 @@ string[string] get()
     return endGet;
 }
 
-string[string] fixedString(string get)
-{
-    get= replace(get, "+", " ");
-    string[string] endGet;
-
-    string[] fixedGet1=split(get, "&"); //
-
-    string[][] fixedGet2;
-
-    foreach(fixedGet;fixedGet1) {
-
-        fixedGet2~=split(fixedGet,"=");        
-        
-    }
-    
-    for(int a=0;a<fixedGet1.length;++a) {
-        for(int b=1;b<fixedGet2[a].length;++b) {
-            //endGet[fixedGet2[a][0]]~=fixedGet2[a][b+1];
-            endGet[fixedGet2[a][0]]~=fixedGet2[a][b];
-        }                
-    }
-    
-    return endGet;
-}
-unittest
-{
-    string[string] deneme;
-    
-    deneme=fixedString("hayvan=at&derece=Orta&sevilen+hayvan=ğüsçü&ikinci+d%C3%BC%C4%9Fme=Ba%C5%9Fka+D%C3%BC%C4%9Fme");
-    assert(deneme["hayvan"]=="at");
-    assert(deneme["sevilen hayvan"]=="ğüşçü");
-
-}
--- main.d
+++ main.d
@@ -3,7 +3,8 @@
  * */
 import library.convertToUTF8;
 import library.get;
+import library.fixedString;
 
 void main(){}
 
-// dmd main.d library/get.d library/convertToUTF8.d
+// dmd main.d library/get.d library/convertToUTF8.d library/fixedString.d
